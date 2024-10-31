58474297f1568e9f76b45281667ecea76b582e4e
jcguirre <joces0480@gmail.com>
2024-10-30 21:31:38 -0500

    Update models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import List, Optional
+ 
+class Usuarios(BaseModel):
+    id: str = Field(..., example='p1')
+    nombre: str= Field(..., example='joel')
+    email: EmailStr = Field(..., example='joel@example.com')
+    edad: int = Field(..., example=30)
+ 
+ 
+class Proyectos(BaseModel):
+    id: str = Field(..., example='e1')
+    nombre: str = Field(..., example='Proyecto 1')
+    Descripcion: Optional[str] = Field(None, example='Proyectos de tecnologia')
+    Id_usuario: List[Usuarios] = Field(default_factory=list)
+    Fecha_Creacion: str = Field(..., example='2024-10-30T20:00:00Z')
