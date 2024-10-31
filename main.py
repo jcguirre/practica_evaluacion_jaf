f9113382401e56820dbfb1b719f7d19085a7250c
jcguirre <joces0480@gmail.com>
2024-10-31 06:51:11 +0000

    init

--- main.py
+++ main.py
@@ -63,3 +63,78 @@ def update_user(user_id: str, update_user: Usuarios):
         raise HTTPException(status_code=404, detail="Usuario No Encontrado")
     except exceptions.CosmosHttpResponseError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+#### Endpoint de Proyectos
+
+##Lista Proyectos
+@app.get("/Proyectos/", response_model=List[Proyectos])
+def list_proyect():
+    
+    try:
+        query = "SELECT * FROM c WHERE 1=1"
+        items = list(Container_Proyectos.query_items(query=query, enable_cross_partition_query=True))
+        return items
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='No se puede listar')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+##Crear Nuevo Proyecto asociado a un usuario
+@app.post("/Proyectos/")
+def crear_proyecto(proyecto:Proyectos):
+
+    try:
+        usuario=Container_Usuarios.read_item(item=proyecto.Id_usuario, partition_key=proyecto.Id_usuario)
+        if not usuario:
+            raise HTTPException(status_code=404,detail="Usuario no encontrado")
+
+        proyecto_dict = proyecto.dict()
+        Container_Proyectos.create_item(proyecto_dict)
+        return {"mensaje":"Proyecto creado exitosamente"}
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='Proyecto no encontrado o Usuario no existe')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+##Obtener proyecto de usuario en específico
+@app.get("/Usuarios/{id_usuario}/Proyectos")
+async def obtener_proyectos_usuario(id_usuario: str):      
+        query = f"SELECT * FROM c WHERE c.Id_usuario ='{id_usuario}'"
+        items = list(Container_Proyectos.query_items(query=query, enable_cross_partition_query=True))
+        return items
+
+
+##Actualizar Proyecto
+@app.put("/Proyectos/{id}", response_model=Proyectos)
+def update_proyect(id: str, update_proyect: Proyectos):
+
+    try:
+        existing_proyect = Container_Proyectos.read_item(item=id, partition_key=id)
+        
+        print(update_proyect.dict(exclude_unset=True))
+        # Actualiza campos
+        existing_proyect.update(update_proyect.dict(exclude_unset=True))
+       
+        Container_Proyectos.replace_item(item=id, body=existing_proyect)
+        return existing_proyect
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="Proyecto No Encontrado")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+##Elimina Proyecto
+@app.delete("/Proyectos/{id}", status_code=204)
+def delete_proyect(id: str):
+    try:
+        Container_Proyectos.delete_item(item=id, partition_key=id)
+        return
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='Proyecto No Encontrado')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
--- models.py
+++ models.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import List, Optional
  
 class Usuarios(BaseModel):
-    id: str = Field(..., example='u1')
+    id: str = Field(..., example='User_1')
     nombre: str= Field(..., example='joel')
     email: EmailStr = Field(..., example='joel@example.com')
     edad: int = Field(..., example=30)
  
  
 class Proyectos(BaseModel):
-    id: str = Field(..., example='e1')
+    id: str = Field(..., example='Proy_1')
     nombre: str = Field(..., example='Proyecto 1')
     Descripcion: Optional[str] = Field(None, example='Proyecto de tecnologia')
-    Id_usuario: List[Usuarios] = Field(default_factory=list)
+    Id_usuario: str = Field(..., example='User_1')
     Fecha_Creacion: str = Field(..., example='2024-10-30T20:00:00Z')
